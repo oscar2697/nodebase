d6c5b7715c6147cd7de7d5982eb0b9ceb83bf83f
oscar2697 <lindooscar635@gmail.com>
2026-01-30 17:26:43 -0500

    Implementing AI providers

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,20 +1,29 @@
 import { inngest } from '@/inngest/client'
-import { createTRPCRouter, protectedProcedure } from '../init'
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init'
 import prisma from '@/lib/db'
+import { google } from '@ai-sdk/google'
+import { generateText } from 'ai'
 
 export const appRouter = createTRPCRouter({
-    getWorkflows: protectedProcedure.query(({ctx}) => {
+    testAi: baseProcedure.mutation(async () => {
+        await inngest.send( {
+            name: 'execute/ai'
+        })
+
+        return { success: true, message: 'Job queued' }
+    }),
+    getWorkflows: protectedProcedure.query(({ ctx }) => {
         return prisma.workflow.findMany()
     }),
     createWorkflow: protectedProcedure.mutation(async () => {
         await inngest.send({
             name: 'test/hello.world',
-            data:{
+            data: {
                 email: 'lindooscar@gmail.com'
             }
         })
 
-        return {success: true, message: 'Job queued'}
+        return { success: true, message: 'Job queued' }
     })
 })
 
